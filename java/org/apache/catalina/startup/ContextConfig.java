3599de6c243fcd2f885edf5130e272a5131a50b6
黄天亮 <2388092952@qq.com>
2024-01-18 11:01:53 +0800

    添加JSP解析器的初始化

--- java/org/apache/catalina/startup/ContextConfig.java
+++ java/org/apache/catalina/startup/ContextConfig.java
@@ -69,6 +69,7 @@ import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.core.StandardHost;
 import org.apache.catalina.util.ContextName;
 import org.apache.catalina.util.Introspection;
+import org.apache.jasper.servlet.JasperInitializer;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.apache.tomcat.Jar;
@@ -985,6 +986,8 @@ public class ContextConfig implements LifecycleListener {
         }
 
         webConfig();
+        // 自己 添加JSP解析器的初始化
+        context.addServletContainerInitializer(new JasperInitializer(), null);
 
         if (!context.getIgnoreAnnotations()) {
             applicationAnnotationsConfig();
